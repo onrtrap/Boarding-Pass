3e7be1cc55fd9ce13f452660413bc9b96bdb0417
Orion Roberts <ornrob@gmail.com>
2022-03-01 13:43:23 -0500

    Better GUI
    Improved GUI, but doesn't take anything yet.

--- Boarding Ticket/src/main/Gui.java
+++ Boarding Ticket/src/main/Gui.java
@@ -10,6 +10,8 @@ public class Gui {
     private int height = 600;
     private JFrame frame;
     private JPanel panel;
+    private JPanel panelCenter;
+    private JLabel label;
     private JTextField name = new JTextField(20);
     private JTextField email = new JTextField(20);
     private JTextField phoneNumber = new JTextField(20);
@@ -23,21 +25,40 @@ public class Gui {
     {
     frame = new JFrame(title);
     panel = new JPanel();
+    panelCenter = new JPanel();
     frame.setSize(width, height);
     frame.setResizable(false);
 
         frame.setDefaultCloseOperation((JFrame.DISPOSE_ON_CLOSE));
-
+    label = new JLabel("Name:");
+    panel.add(label);
     panel.add(name);
+    label = new JLabel("Email:");
+    panel.add(label);
         panel.add(email);
+        label = new JLabel("Phone #:");
+        panel.add(label);
         panel.add(phoneNumber);
-        panel.add(gender);
-        panel.add(age);
-        panel.add(date);
-        panel.add(destination);
-        panel.add(departureTime);
+        label = new JLabel("Gender:");
+        panelCenter.add(label);
+        panelCenter.add(gender);
+        label = new JLabel("Age:");
+        panelCenter.add(label);
+        panelCenter.add(age);
+        label = new JLabel("Date:");
+        panelCenter.add(label);
+        panelCenter.add(date);
+        label = new JLabel("Destination:");
+        panelCenter.add(label);
+        panelCenter.add(destination);
+        label = new JLabel("Depart.Time");
+        panelCenter.add(label);
+        panelCenter.add(departureTime);
+        //panel.add(submit);
         panel.setBackground(Color.CYAN);
-        name.setBounds(25,25, 75, 25);
+        panelCenter.setBackground(Color.CYAN);
+
+     /**   name.setBounds(25,25, 75, 25);
 
         email.setBounds(25, 75, 75, 25);
 
@@ -52,19 +73,25 @@ public class Gui {
         destination.setBounds(25,325, 75,25);
 
         departureTime.setBounds(25,375, 75,25);
+
+        submit.setBounds(775, 225, 50, 25);
+    **/
         //panel.setLocation(5,5);
-    frame.add(panel);
-    /**frame.add(name);
-    frame.add(email);
-    frame.add(phoneNumber);
-    frame.add(gender);
-    frame.add(age);
-    frame.add(date);
-    frame.add(destination);
-    frame.add(departureTime);
-    frame.setBackground(Color.CYAN);
-    frame.setLocation(5,5);
-**/
+    //frame.add(panel);
+        /**frame.getContentPane().add(name);
+        frame.getContentPane().add(email);
+        frame.getContentPane().add(phoneNumber);
+        frame.getContentPane().add(gender);
+        frame.getContentPane().add(age);
+        frame.getContentPane().add(date);
+        frame.getContentPane().add(destination);
+        frame.getContentPane().add(departureTime);
+        frame.getContentPane().add(submit);
+        frame.getContentPane().setBackground(Color.CYAN);
+    frame.setLocation(5,5);**/
+        frame.getContentPane().add(BorderLayout.NORTH, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, submit);
+        frame.getContentPane().add(BorderLayout.CENTER, panelCenter);
 
     //frame.setLayout(new FlowLayout());
     //frame.pack();
